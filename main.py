452088c111028d2bf732f9b8733c9b16203c3342
groenj-blitz <groenj@wsc.school.nz>
2026-07-01 15:04:28 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print ("hello")
+print ("Quiz on NZ")
+start_code = input ("press enter to start")
+print ("let's begin")
+
+print ("question: what is the capital of nz")
+answer = input ("Is the answer a: auckland b: wellington c:christchurch")
+print ("this is your answer:" + answer)
+if answer == "a":
+    print ("correct")
+if answer == "b" or "c":
+    print ("incorrect")
+
+
+print ("what's nzs other name")
+answer1 = input ("is it a: aoeteroa b: russia c: waitangi")
+print ("this is your answer:" + answer1)
+if answer1 == "a":
+    print ("correct")
+if answer1 == "b" or "c":
+    print ("incorrect")
+
+print ("who lived in NZ before the british")
+answer2 = input ("is it a: maori b: nazi's c: english")
+print ("this is your answer:" + answer2)
+if answer2 == "a":
+    print ("correct")
+if answer2 == "b" or "c":
+    print ("incorrect")
